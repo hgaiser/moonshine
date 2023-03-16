f8c57b446cc6bb606b127c3b30b8b50fb65c7305
Hans Gaiser <hansg91@gmail.com>
2023-03-16 14:34:32 +0100

    Update ffmpeg bindings.

--- ffmpeg-sys/src/lib.rs
+++ ffmpeg-sys/src/lib.rs
@@ -3,6 +3,8 @@
 #![allow(non_snake_case)]
 
 mod generated;
+use std::ptr::null_mut;
+
 pub use generated::*;
 
 // Pixel formats
@@ -62,3 +64,109 @@ pub const AVERROR_HTTP_FORBIDDEN: i32     = fferrtag(0xF8 as char,'4','0','3');
 pub const AVERROR_HTTP_NOT_FOUND: i32     = fferrtag(0xF8 as char,'4','0','4');
 pub const AVERROR_HTTP_OTHER_4XX: i32     = fferrtag(0xF8 as char,'4','X','X');
 pub const AVERROR_HTTP_SERVER_ERROR: i32  = fferrtag(0xF8 as char,'5','X','X');
+
+// Channel layout
+
+pub const fn av_channel_layout_mask(nb: i32, m: u64) -> AVChannelLayout {
+	AVChannelLayout {
+		order: AVChannelOrder_AV_CHANNEL_ORDER_NATIVE,
+		nb_channels: nb,
+		u: AVChannelLayout__bindgen_ty_1 { mask: m },
+		opaque: null_mut(),
+	}
+}
+
+pub const AV_CH_FRONT_LEFT: u64            = 1u64 << AVChannel_AV_CHAN_FRONT_LEFT;
+pub const AV_CH_FRONT_RIGHT: u64           = 1u64 << AVChannel_AV_CHAN_FRONT_RIGHT;
+pub const AV_CH_FRONT_CENTER: u64          = 1u64 << AVChannel_AV_CHAN_FRONT_CENTER;
+pub const AV_CH_LOW_FREQUENCY: u64         = 1u64 << AVChannel_AV_CHAN_LOW_FREQUENCY;
+pub const AV_CH_BACK_LEFT: u64             = 1u64 << AVChannel_AV_CHAN_BACK_LEFT;
+pub const AV_CH_BACK_RIGHT: u64            = 1u64 << AVChannel_AV_CHAN_BACK_RIGHT;
+pub const AV_CH_FRONT_LEFT_OF_CENTER: u64  = 1u64 << AVChannel_AV_CHAN_FRONT_LEFT_OF_CENTER;
+pub const AV_CH_FRONT_RIGHT_OF_CENTER: u64 = 1u64 << AVChannel_AV_CHAN_FRONT_RIGHT_OF_CENTER;
+pub const AV_CH_BACK_CENTER: u64           = 1u64 << AVChannel_AV_CHAN_BACK_CENTER;
+pub const AV_CH_SIDE_LEFT: u64             = 1u64 << AVChannel_AV_CHAN_SIDE_LEFT;
+pub const AV_CH_SIDE_RIGHT: u64            = 1u64 << AVChannel_AV_CHAN_SIDE_RIGHT;
+pub const AV_CH_TOP_CENTER: u64            = 1u64 << AVChannel_AV_CHAN_TOP_CENTER;
+pub const AV_CH_TOP_FRONT_LEFT: u64        = 1u64 << AVChannel_AV_CHAN_TOP_FRONT_LEFT;
+pub const AV_CH_TOP_FRONT_CENTER: u64      = 1u64 << AVChannel_AV_CHAN_TOP_FRONT_CENTER;
+pub const AV_CH_TOP_FRONT_RIGHT: u64       = 1u64 << AVChannel_AV_CHAN_TOP_FRONT_RIGHT;
+pub const AV_CH_TOP_BACK_LEFT: u64         = 1u64 << AVChannel_AV_CHAN_TOP_BACK_LEFT;
+pub const AV_CH_TOP_BACK_CENTER: u64       = 1u64 << AVChannel_AV_CHAN_TOP_BACK_CENTER;
+pub const AV_CH_TOP_BACK_RIGHT: u64        = 1u64 << AVChannel_AV_CHAN_TOP_BACK_RIGHT;
+pub const AV_CH_STEREO_LEFT: u64           = 1u64 << AVChannel_AV_CHAN_STEREO_LEFT;
+pub const AV_CH_STEREO_RIGHT: u64          = 1u64 << AVChannel_AV_CHAN_STEREO_RIGHT;
+pub const AV_CH_WIDE_LEFT: u64             = 1u64 << AVChannel_AV_CHAN_WIDE_LEFT;
+pub const AV_CH_WIDE_RIGHT: u64            = 1u64 << AVChannel_AV_CHAN_WIDE_RIGHT;
+pub const AV_CH_SURROUND_DIRECT_LEFT: u64  = 1u64 << AVChannel_AV_CHAN_SURROUND_DIRECT_LEFT;
+pub const AV_CH_SURROUND_DIRECT_RIGHT: u64 = 1u64 << AVChannel_AV_CHAN_SURROUND_DIRECT_RIGHT;
+pub const AV_CH_LOW_FREQUENCY_2: u64       = 1u64 << AVChannel_AV_CHAN_LOW_FREQUENCY_2;
+pub const AV_CH_TOP_SIDE_LEFT: u64         = 1u64 << AVChannel_AV_CHAN_TOP_SIDE_LEFT;
+pub const AV_CH_TOP_SIDE_RIGHT: u64        = 1u64 << AVChannel_AV_CHAN_TOP_SIDE_RIGHT;
+pub const AV_CH_BOTTOM_FRONT_CENTER: u64   = 1u64 << AVChannel_AV_CHAN_BOTTOM_FRONT_CENTER;
+pub const AV_CH_BOTTOM_FRONT_LEFT: u64     = 1u64 << AVChannel_AV_CHAN_BOTTOM_FRONT_LEFT;
+pub const AV_CH_BOTTOM_FRONT_RIGHT: u64    = 1u64 << AVChannel_AV_CHAN_BOTTOM_FRONT_RIGHT;
+
+pub const AV_CH_LAYOUT_MONO: u64              = AV_CH_FRONT_CENTER;
+pub const AV_CH_LAYOUT_STEREO: u64            = AV_CH_FRONT_LEFT|AV_CH_FRONT_RIGHT;
+pub const AV_CH_LAYOUT_2POINT1: u64           = AV_CH_LAYOUT_STEREO|AV_CH_LOW_FREQUENCY;
+pub const AV_CH_LAYOUT_2_1: u64               = AV_CH_LAYOUT_STEREO|AV_CH_BACK_CENTER;
+pub const AV_CH_LAYOUT_SURROUND: u64          = AV_CH_LAYOUT_STEREO|AV_CH_FRONT_CENTER;
+pub const AV_CH_LAYOUT_3POINT1: u64           = AV_CH_LAYOUT_SURROUND|AV_CH_LOW_FREQUENCY;
+pub const AV_CH_LAYOUT_4POINT0: u64           = AV_CH_LAYOUT_SURROUND|AV_CH_BACK_CENTER;
+pub const AV_CH_LAYOUT_4POINT1: u64           = AV_CH_LAYOUT_4POINT0|AV_CH_LOW_FREQUENCY;
+pub const AV_CH_LAYOUT_2_2: u64               = AV_CH_LAYOUT_STEREO|AV_CH_SIDE_LEFT|AV_CH_SIDE_RIGHT;
+pub const AV_CH_LAYOUT_QUAD: u64              = AV_CH_LAYOUT_STEREO|AV_CH_BACK_LEFT|AV_CH_BACK_RIGHT;
+pub const AV_CH_LAYOUT_5POINT0: u64           = AV_CH_LAYOUT_SURROUND|AV_CH_SIDE_LEFT|AV_CH_SIDE_RIGHT;
+pub const AV_CH_LAYOUT_5POINT1: u64           = AV_CH_LAYOUT_5POINT0|AV_CH_LOW_FREQUENCY;
+pub const AV_CH_LAYOUT_5POINT0_BACK: u64      = AV_CH_LAYOUT_SURROUND|AV_CH_BACK_LEFT|AV_CH_BACK_RIGHT;
+pub const AV_CH_LAYOUT_5POINT1_BACK: u64      = AV_CH_LAYOUT_5POINT0_BACK|AV_CH_LOW_FREQUENCY;
+pub const AV_CH_LAYOUT_6POINT0: u64           = AV_CH_LAYOUT_5POINT0|AV_CH_BACK_CENTER;
+pub const AV_CH_LAYOUT_6POINT0_FRONT: u64     = AV_CH_LAYOUT_2_2|AV_CH_FRONT_LEFT_OF_CENTER|AV_CH_FRONT_RIGHT_OF_CENTER;
+pub const AV_CH_LAYOUT_HEXAGONAL: u64         = AV_CH_LAYOUT_5POINT0_BACK|AV_CH_BACK_CENTER;
+pub const AV_CH_LAYOUT_6POINT1: u64           = AV_CH_LAYOUT_5POINT1|AV_CH_BACK_CENTER;
+pub const AV_CH_LAYOUT_6POINT1_BACK: u64      = AV_CH_LAYOUT_5POINT1_BACK|AV_CH_BACK_CENTER;
+pub const AV_CH_LAYOUT_6POINT1_FRONT: u64     = AV_CH_LAYOUT_6POINT0_FRONT|AV_CH_LOW_FREQUENCY;
+pub const AV_CH_LAYOUT_7POINT0: u64           = AV_CH_LAYOUT_5POINT0|AV_CH_BACK_LEFT|AV_CH_BACK_RIGHT;
+pub const AV_CH_LAYOUT_7POINT0_FRONT: u64     = AV_CH_LAYOUT_5POINT0|AV_CH_FRONT_LEFT_OF_CENTER|AV_CH_FRONT_RIGHT_OF_CENTER;
+pub const AV_CH_LAYOUT_7POINT1: u64           = AV_CH_LAYOUT_5POINT1|AV_CH_BACK_LEFT|AV_CH_BACK_RIGHT;
+pub const AV_CH_LAYOUT_7POINT1_WIDE: u64      = AV_CH_LAYOUT_5POINT1|AV_CH_FRONT_LEFT_OF_CENTER|AV_CH_FRONT_RIGHT_OF_CENTER;
+pub const AV_CH_LAYOUT_7POINT1_WIDE_BACK: u64 = AV_CH_LAYOUT_5POINT1_BACK|AV_CH_FRONT_LEFT_OF_CENTER|AV_CH_FRONT_RIGHT_OF_CENTER;
+pub const AV_CH_LAYOUT_7POINT1_TOP_BACK: u64  = AV_CH_LAYOUT_5POINT1_BACK|AV_CH_TOP_FRONT_LEFT|AV_CH_TOP_FRONT_RIGHT;
+pub const AV_CH_LAYOUT_OCTAGONAL: u64         = AV_CH_LAYOUT_5POINT0|AV_CH_BACK_LEFT|AV_CH_BACK_CENTER|AV_CH_BACK_RIGHT;
+pub const AV_CH_LAYOUT_CUBE: u64              = AV_CH_LAYOUT_QUAD|AV_CH_TOP_FRONT_LEFT|AV_CH_TOP_FRONT_RIGHT|AV_CH_TOP_BACK_LEFT|AV_CH_TOP_BACK_RIGHT;
+pub const AV_CH_LAYOUT_HEXADECAGONAL: u64     = AV_CH_LAYOUT_OCTAGONAL|AV_CH_WIDE_LEFT|AV_CH_WIDE_RIGHT|AV_CH_TOP_BACK_LEFT|AV_CH_TOP_BACK_RIGHT|AV_CH_TOP_BACK_CENTER|AV_CH_TOP_FRONT_CENTER|AV_CH_TOP_FRONT_LEFT|AV_CH_TOP_FRONT_RIGHT;
+pub const AV_CH_LAYOUT_STEREO_DOWNMIX: u64    = AV_CH_STEREO_LEFT|AV_CH_STEREO_RIGHT;
+pub const AV_CH_LAYOUT_22POINT2: u64          = AV_CH_LAYOUT_5POINT1_BACK|AV_CH_FRONT_LEFT_OF_CENTER|AV_CH_FRONT_RIGHT_OF_CENTER|AV_CH_BACK_CENTER|AV_CH_LOW_FREQUENCY_2|AV_CH_SIDE_LEFT|AV_CH_SIDE_RIGHT|AV_CH_TOP_FRONT_LEFT|AV_CH_TOP_FRONT_RIGHT|AV_CH_TOP_FRONT_CENTER|AV_CH_TOP_CENTER|AV_CH_TOP_BACK_LEFT|AV_CH_TOP_BACK_RIGHT|AV_CH_TOP_SIDE_LEFT|AV_CH_TOP_SIDE_RIGHT|AV_CH_TOP_BACK_CENTER|AV_CH_BOTTOM_FRONT_CENTER|AV_CH_BOTTOM_FRONT_LEFT|AV_CH_BOTTOM_FRONT_RIGHT;
+
+pub const AV_CHANNEL_LAYOUT_MONO: AVChannelLayout              = av_channel_layout_mask(1, AV_CH_LAYOUT_MONO);
+pub const AV_CHANNEL_LAYOUT_STEREO: AVChannelLayout            = av_channel_layout_mask(2,  AV_CH_LAYOUT_STEREO);
+pub const AV_CHANNEL_LAYOUT_2POINT1: AVChannelLayout           = av_channel_layout_mask(3,  AV_CH_LAYOUT_2POINT1);
+pub const AV_CHANNEL_LAYOUT_2_1: AVChannelLayout               = av_channel_layout_mask(3,  AV_CH_LAYOUT_2_1);
+pub const AV_CHANNEL_LAYOUT_SURROUND: AVChannelLayout          = av_channel_layout_mask(3,  AV_CH_LAYOUT_SURROUND);
+pub const AV_CHANNEL_LAYOUT_3POINT1: AVChannelLayout           = av_channel_layout_mask(4,  AV_CH_LAYOUT_3POINT1);
+pub const AV_CHANNEL_LAYOUT_4POINT0: AVChannelLayout           = av_channel_layout_mask(4,  AV_CH_LAYOUT_4POINT0);
+pub const AV_CHANNEL_LAYOUT_4POINT1: AVChannelLayout           = av_channel_layout_mask(5,  AV_CH_LAYOUT_4POINT1);
+pub const AV_CHANNEL_LAYOUT_2_2: AVChannelLayout               = av_channel_layout_mask(4,  AV_CH_LAYOUT_2_2);
+pub const AV_CHANNEL_LAYOUT_QUAD: AVChannelLayout              = av_channel_layout_mask(4,  AV_CH_LAYOUT_QUAD);
+pub const AV_CHANNEL_LAYOUT_5POINT0: AVChannelLayout           = av_channel_layout_mask(5,  AV_CH_LAYOUT_5POINT0);
+pub const AV_CHANNEL_LAYOUT_5POINT1: AVChannelLayout           = av_channel_layout_mask(6,  AV_CH_LAYOUT_5POINT1);
+pub const AV_CHANNEL_LAYOUT_5POINT0_BACK: AVChannelLayout      = av_channel_layout_mask(5,  AV_CH_LAYOUT_5POINT0_BACK);
+pub const AV_CHANNEL_LAYOUT_5POINT1_BACK: AVChannelLayout      = av_channel_layout_mask(6,  AV_CH_LAYOUT_5POINT1_BACK);
+pub const AV_CHANNEL_LAYOUT_6POINT0: AVChannelLayout           = av_channel_layout_mask(6,  AV_CH_LAYOUT_6POINT0);
+pub const AV_CHANNEL_LAYOUT_6POINT0_FRONT: AVChannelLayout     = av_channel_layout_mask(6,  AV_CH_LAYOUT_6POINT0_FRONT);
+pub const AV_CHANNEL_LAYOUT_HEXAGONAL: AVChannelLayout         = av_channel_layout_mask(6,  AV_CH_LAYOUT_HEXAGONAL);
+pub const AV_CHANNEL_LAYOUT_6POINT1: AVChannelLayout           = av_channel_layout_mask(7,  AV_CH_LAYOUT_6POINT1);
+pub const AV_CHANNEL_LAYOUT_6POINT1_BACK: AVChannelLayout      = av_channel_layout_mask(7,  AV_CH_LAYOUT_6POINT1_BACK);
+pub const AV_CHANNEL_LAYOUT_6POINT1_FRONT: AVChannelLayout     = av_channel_layout_mask(7,  AV_CH_LAYOUT_6POINT1_FRONT);
+pub const AV_CHANNEL_LAYOUT_7POINT0: AVChannelLayout           = av_channel_layout_mask(7,  AV_CH_LAYOUT_7POINT0);
+pub const AV_CHANNEL_LAYOUT_7POINT0_FRONT: AVChannelLayout     = av_channel_layout_mask(7,  AV_CH_LAYOUT_7POINT0_FRONT);
+pub const AV_CHANNEL_LAYOUT_7POINT1: AVChannelLayout           = av_channel_layout_mask(8,  AV_CH_LAYOUT_7POINT1);
+pub const AV_CHANNEL_LAYOUT_7POINT1_WIDE: AVChannelLayout      = av_channel_layout_mask(8,  AV_CH_LAYOUT_7POINT1_WIDE);
+pub const AV_CHANNEL_LAYOUT_7POINT1_WIDE_BACK: AVChannelLayout = av_channel_layout_mask(8,  AV_CH_LAYOUT_7POINT1_WIDE_BACK);
+pub const AV_CHANNEL_LAYOUT_7POINT1_TOP_BACK: AVChannelLayout  = av_channel_layout_mask(8,  AV_CH_LAYOUT_7POINT1_TOP_BACK);
+pub const AV_CHANNEL_LAYOUT_OCTAGONAL: AVChannelLayout         = av_channel_layout_mask(8,  AV_CH_LAYOUT_OCTAGONAL);
+pub const AV_CHANNEL_LAYOUT_CUBE: AVChannelLayout              = av_channel_layout_mask(8,  AV_CH_LAYOUT_CUBE);
+pub const AV_CHANNEL_LAYOUT_HEXADECAGONAL: AVChannelLayout     = av_channel_layout_mask(16, AV_CH_LAYOUT_HEXADECAGONAL);
+pub const AV_CHANNEL_LAYOUT_STEREO_DOWNMIX: AVChannelLayout    = av_channel_layout_mask(2,  AV_CH_LAYOUT_STEREO_DOWNMIX);
+pub const AV_CHANNEL_LAYOUT_22POINT2: AVChannelLayout          = av_channel_layout_mask(24, AV_CH_LAYOUT_22POINT2);
