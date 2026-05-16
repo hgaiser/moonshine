7a15665098d551f5ee50e8be2d8310a069bf3dad
Hans Gaiser <hansg91@gmail.com>
2026-05-16 16:07:07 +0200

    Move certificate creation to its own function.

--- src/main.rs
+++ src/main.rs
@@ -139,7 +139,40 @@ impl Moonshine {
 	pub async fn new(config: Config, shutdown: ShutdownManager<i32>) -> Result<Self, ()> {
 		let state = State::new().await?;
 
-		let (cert, pkey) = if !config.webserver.certificate.exists() && !config.webserver.private_key.exists() {
+		let (cert, pkey) = Self::load_or_create_certificate(&config).await?;
+
+		// Create a manager for interacting with sessions.
+		let session_manager = SessionManager::new(config.clone(), shutdown.clone())?;
+
+		// Create a manager for saving and loading client state.
+		let client_manager = ClientManager::new(state.clone(), cert.clone(), pkey, shutdown.trigger_shutdown_token(3));
+
+		// Run the RTSP server.
+		let rtsp_server = RtspServer::new(config.clone(), session_manager.clone(), shutdown.clone());
+
+		// Publish the Moonshine service using zeroconf.
+		publisher::spawn(config.webserver.port, config.name.clone());
+
+		// Create a handler for the webserver.
+		let webserver = Webserver::new(
+			config,
+			state.get_uuid().await?,
+			cert,
+			client_manager.clone(),
+			session_manager.clone(),
+			shutdown,
+		)?;
+
+		Ok(Self {
+			_rtsp_server: rtsp_server,
+			_session_manager: session_manager,
+			_client_manager: client_manager,
+			_webserver: webserver,
+		})
+	}
+
+	async fn load_or_create_certificate(config: &Config) -> Result<(String, String), ()> {
+		if !config.webserver.certificate.exists() && !config.webserver.private_key.exists() {
 			tracing::info!("No certificate found, creating a new one.");
 
 			let (cert, pkey) =
@@ -174,7 +207,7 @@ impl Moonshine {
 			tracing::debug!("Saved private key to {}", config.webserver.certificate.display());
 			tracing::debug!("Saved certificate to {}", config.webserver.private_key.display());
 
-			(cert, pkey)
+			Ok((cert, pkey))
 		} else {
 			let cert = std::fs::read_to_string(&config.webserver.certificate)
 				.map_err(|e| tracing::error!("Failed to read server certificate: {e}"))?;
@@ -182,36 +215,7 @@ impl Moonshine {
 			let pkey = std::fs::read_to_string(&config.webserver.private_key)
 				.map_err(|e| tracing::error!("Failed to read private key: {e}"))?;
 
-			(cert, pkey)
-		};
-
-		// Create a manager for interacting with sessions.
-		let session_manager = SessionManager::new(config.clone(), shutdown.clone())?;
-
-		// Create a manager for saving and loading client state.
-		let client_manager = ClientManager::new(state.clone(), cert.clone(), pkey, shutdown.trigger_shutdown_token(3));
-
-		// Run the RTSP server.
-		let rtsp_server = RtspServer::new(config.clone(), session_manager.clone(), shutdown.clone());
-
-		// Publish the Moonshine service using zeroconf.
-		publisher::spawn(config.webserver.port, config.name.clone());
-
-		// Create a handler for the webserver.
-		let webserver = Webserver::new(
-			config,
-			state.get_uuid().await?,
-			cert,
-			client_manager.clone(),
-			session_manager.clone(),
-			shutdown,
-		)?;
-
-		Ok(Self {
-			_rtsp_server: rtsp_server,
-			_session_manager: session_manager,
-			_client_manager: client_manager,
-			_webserver: webserver,
-		})
+			Ok((cert, pkey))
+		}
 	}
 }
